f6a7ca421b84086aa9a0660f239dde3f697ed889
Zlabji <extradisk@yahoo.com>
2021-03-19 18:48:49 -0400

    Update docker_publish.sh

--- converter/docker_publish.sh
+++ converter/docker_publish.sh
@@ -1,1 +1,1 @@
- docker push docker.pkg.github.com/sap-samples/teched2020-developer-keynote/calculationservice:latest
+ docker push docker.pkg.github.com/hbrahi1/teched2020-developer-keynote/calculationservice:latest
